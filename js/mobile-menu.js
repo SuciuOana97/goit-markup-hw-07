ac9cbc28d647493c350091c6345ce6d5253e59a7
Oanasuciu97 <luciana_bodeaoana@yahoo.com>
2024-01-22 21:02:06 +0100

    @media (min-width: 768px) and (max-width: 1158px)

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,28 +1,14 @@
-//menu for mobile
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[js-open-menu]"),
+    closeModalBtn: document.querySelector("[js-close-menu]"),
+    modal: document.querySelector("[js-menu-container]"),
+  };
 
-const hamburger = document.querySelector(".hamburger");
-const list = document.querySelector(".list");
-const addressMenu = document.querySelector(".address-menu");
-const socialMedia = document.querySelector(".social-media");
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  list.classList.toggle("active");
-  addressMenu.classList.toggle("active");
-  socialMedia.classList.toggle("active");
-});
-
-document.querySelectorAll("list-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    list.classList.remove("active");
-  })
-);
-
-document.querySelectorAll("address-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    addressMenu.classList.remove("active");
-    socialMedia.classList.toggle("active");
-  })
-);
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
